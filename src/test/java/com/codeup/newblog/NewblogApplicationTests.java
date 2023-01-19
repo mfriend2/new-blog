5124032a7730ebe3ceec21c402c71c61c7fd336d
Michael Friend <michael.l.friend2@gmail.com>
2023-01-19 14:53:39 -0600

    first commit

---
+++ src/test/java/com/codeup/newblog/NewblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.newblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
