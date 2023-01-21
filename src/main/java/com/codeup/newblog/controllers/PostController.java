ef0d81fe81a0c5d0c7891b99c20fda4139389f57
Michael Friend <michael.l.friend2@gmail.com>
2023-01-20 20:58:22 -0600

    updated

---
+++ src/main/java/com/codeup/newblog/controllers/PostController.java
@@ -0,0 +1,38 @@
+package com.codeup.newblog.controllers;
+
+import com.codeup.newblog.models.Post;
+import com.codeup.newblog.repositories.PostRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.text.ParseException;
+
+@Controller
+public class PostController {
+    private PostRepository postsDao;
+
+    public PostController(PostRepository postsDao) {
+        this.postsDao = postsDao;
+    }
+
+    @GetMapping("posts")
+    public String postsIndex(Model model) throws ParseException {
+        model.addAttribute("posts", postsDao.findAll());
+        return "posts/index";
+    }
+
+    @GetMapping("posts/create")
+    public String showCreatePost(Model model) {
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+
+    @PostMapping("posts/create")
+    public String createPost(@ModelAttribute Post post) {
+        postsDao.save(post);
+        return "redirect:/posts";
+    }
+}
---
+++ src/main/java/com/codeup/newblog/repositories/PostRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.newblog.repositories;
+
+import com.codeup.newblog.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
